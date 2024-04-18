7ac9857e44b5a965b507163d88fd4fe90500ea40
Thiago <114115392+thiagobernardosantos@users.noreply.github.com>
2024-04-18 17:22:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("ESSE SITE TEM A MELHOR EXPERIENCIA EM COMPUTADORES/NOTEBOOKS!")
+
+const saco = document.getElementById('saco');
+const favo = document.getElementById('favo');
+saco.style.opacity = '0'; 
+favo.style.opacity = '0'; 
+
+const saco1 = document.getElementById('saco1');
+const favo1 = document.getElementById('favo1');
+saco1.style.opacity = '0'; 
+favo1.style.opacity = '0'; 
+
+const saco2 = document.getElementById('saco2');
+const favo2 = document.getElementById('favo2');
+saco2.style.opacity = '0'; 
+favo2.style.opacity = '0'; 
